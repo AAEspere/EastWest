11d554bcd4855070f5b036a7bff4c1f2f3749771
gb1962 <gb1962@wayne.edu>
2020-06-19 11:46:02 -0400

    Added new slides for the multistep form. Now going to implement the javascript and css styling

--- contactForm.php
+++ contactForm.php
@@ -3,7 +3,8 @@
 require('recaptcha-master/src/autoload.php');
 
 //information for the contact form email
-$emailto = 'eastwesthomecare@eastwesthealthservices.com';
+//eastwesthomecare@eastwesthealthservices.com
+$emailto = 'aespere0@gmail.com';
 $emailfrom = $_POST['contactEmail'];
 $fromname = $_POST['contactName'];
 $phone = $_POST['contactMobile'];
@@ -17,7 +18,7 @@ try {
         
         //validate recaptcha
         
-        if (!isset($_POST['g-recaptcha-response'])) {
+       /* if (!isset($_POST['g-recaptcha-response'])) {
             throw new \Exception('ReCaptcha is not set.');
         }
         
@@ -28,6 +29,7 @@ try {
         if (!$response->isSuccess()) {
             throw new \Exception('ReCaptcha was not validated.');
         }
+        */
         
         //the actual message of the email that will be sent
         $messagebody = 'CONTACT US FORM INFORMATION'. "\n".
@@ -58,41 +60,7 @@ try {
     }
     
 }
-
     catch (\Exception $e) {
     $responseArray = array('type' => 'danger', 'message' => $e->getMessage());
     }
-
-/*  OLD CODE
-
-	$emailto = 'eastwesthomecare@eastwesthealthservices.com';
-	$emailfrom = $_POST['contactEmail'];
-	$fromname = $_POST['contactName'];
-	$phone = $_POST['contactMobile'];
-	$message = $_POST['contactSubject'];
-	
-	$subject = 'Contact Form Information';
-	
-	$messagebody = 'CONTACT US FORM INFORMATION'. "\n".
-	    
-	    
-	                'Name: '.$fromname. "\n".
-	                'Email: '.$emailfrom. "\n".
-	                    "Phone: ".$phone. "\n".
-	                    "Message: ".$message. "\n";
-	
-	$headers = 
-		//'Return-Path: ' . $emailfrom . "\r\n" . 
-		//'From: ' . $fromname . ' <' . $emailfrom . '>' . "\r\n" . 
-		'X-MSMail-Priority: High' . "\r\n" . 
-		'X-Mailer: PHP ' . phpversion() .  "\r\n" . 
-		//'Reply-To: ' . $fromname . ' <' . $emailfrom . '>' . "\r\n" .
-		'MIME-Version: 1.0' . "\r\n" . 
-		'Content-Transfer-Encoding: 8bit' . "\r\n" . 
-		'Content-Type: text/plain; charset=UTF-8' . "\r\n";
-	//$params = '-f ' . $emailfrom;
-	$test = mail($emailto, $subject, $messagebody, $headers); // $params);
-	header('Location: contact.html');
-	// $test should be TRUE if the mail function is called correctly
-*/
 ?>
